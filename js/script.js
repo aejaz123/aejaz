e7fb74865e9c630a5f775e25d86364cac2da0e1f
aejaz123 <gaziaejaz@gmail.com>
2017-04-11 15:23:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
@@ -6,6 +5,8 @@ document.addEventListener("DOMContentLoaded",
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
+           var array=new array(1,2,3,4,5,6,7,8,9,10};
+           int i;
         
         // Call server to get the name
         $ajaxUtils
@@ -13,11 +14,11 @@ document.addEventListener("DOMContentLoaded",
             function (res) {
               var message = 
                 res.firstName + " " + res.lastName
-              if (res.likesChineseFood) {
-                message += " likes Chinese food";
+              for(i=1;i<=10;i++) {
+                message += " name is: ";
               }
               else {
-                message += " doesn't like Chinese food";
+                message += " Invalid number ";
               }
               message += " and uses ";
               message += res.numberOfDisplays + 1;
